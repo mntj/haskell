f954251667b5f2966cabb0c171c0da0a2e7d98cb
mntj <mntj11@gmail.com>
2014-12-25 10:52:25 -0500

    More haskell

--- main.hs
+++ main.hs
@@ -1,3 +1,11 @@
 doubleSmallNumber x = if x > 100
                         then x
                         else x * 2
+
+[1,2,3,4] ++ [9,10,11,12]
+
+"hello" ++ " " ++ "world"
+
+"Saturday" !! 6
+
+[3,2,1] > [2,1,0]
