4cfe0282b1a102c05ff5920ad8460f177642a873
mntj <mntj11@gmail.com>
2014-12-27 12:05:13 -0500

    Add functions, lists, and operators

--- functions.hs
+++ functions.hs
@@ -1,11 +1,14 @@
+succ 8
+
+min 9 10
+max 10 12
+
+doubleMe x = x + x
+
+doubleUs x y = doubleMe x + doubleMe y
+
 doubleSmallNumber x = if x > 100
                         then x
                         else x * 2
 
-[1,2,3,4] ++ [9,10,11,12]
 
-"hello" ++ " " ++ "world"
-
-"Saturday" !! 6
-
-[3,2,1] > [2,1,0]
---
+++ operators.hs
@@ -0,0 +1,19 @@
+2 + 15
+37 * 25
+18 - 3
+87 / 3
+
+True && False
+False || True
+not False
+not (True && True)
+
+5 == 5
+1 == 0
+
+-- Inequalities
+
+5 /= 5
+5 /= 4
+
+"hello" == "hello"
