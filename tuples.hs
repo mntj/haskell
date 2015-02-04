4f2eab64a69c3a88147d7f6f1ddf5c08ea147b95
mntj <mntj11@gmail.com>
2015-02-03 22:09:12 -0500

    More tuples and infinite lists

--- tuples.hs
+++ tuples.hs
@@ -12,3 +12,10 @@ snd ("Wow", False)
 
 zip [1,2,3,4,5] [5,5,5,5,5]
 -- [(1,5),(2,5),(3,5),(4,5),(5,5)]
+
+zip [5,3,2,6,2,7,2,5,4,6,6] ["im", "a", "turtle"]
+-- [(5,"im"),(3,"a"),(2,"turtle")]
+
+-- Finite with infinite list:
+zip [1..] ["apple", "orange", "cherry", "mango"]
+-- [(1,"apple"),(2,"orange"),(3,"cherry"),(4,"mango")]
